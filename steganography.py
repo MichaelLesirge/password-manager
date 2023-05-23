44ba079140eb66dc2c2e5ffdf64ab95cd3f1b9c4
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2023-05-23 00:48:14 -0700

    start work on decode funtion

--- steganography.py
+++ steganography.py
@@ -1,33 +1,69 @@
+# thanks Computerphile. https://youtu.be/TWEXCYQKyDc
 
 class Config:
-    STOP_CODE = b'#####'
+    STOP_CODE = b'\0'
 
 def to_binary(data: bytes | int) -> str:
-    if isinstance(data, int):
-        return format(data, "08b")
-    return "".join(to_binary(i) for i in data)
+    return "".join(format(i, "08b") for i in data)
 
-def encode(image: bytes, secret_data: bytes) -> bytes:
-    new_image = bytearray(image)
-    secret_data += Config.STOP_CODE
+def get_png_body_position(image: bytes) -> tuple[int, int]:
+    try:
+        png_body_start = image.index(b'IDAT') + 4
+    except:
+        raise Exception("Invalid PNG file. Unable to find IDAT marker.")
     
-    if len(secret_data) > len(new_image) / 8:
-        raise Exception(f"The {len(secret_data)} bytes of data could not fit in the {len(new_image) / 8} byte avialble bytes of the {len(new_image)} byte image. Need bigger image.")
+    try:
+        png_body_end = image.index(b'IEND')
+    except ValueError:
+        raise Exception("Invalid PNG file. Unable to find IEND marker.")
+
+    return png_body_start, png_body_end
+
+def encode(base_png_image: bytes, secret_data: bytes) -> bytes:
+    image = bytearray(base_png_image)
+    
+    secret_data_binary = to_binary(secret_data + Config.STOP_CODE)
+    
+    start_index, end_index = get_png_body_position(image) 
+    
+    free_bytes = (end_index-start_index-len(to_binary(Config.STOP_CODE))) // 8
+    if len(secret_data_binary) > free_bytes:
+        raise Exception(f"Need bigger image to store all data. Currently have {free_bytes} free bytes,")
+    
+    for i, bit in enumerate(secret_data_binary):
+        byte_index = end_index - i - 8
+        image[byte_index] = (image[byte_index] & 0b11111110) | int(bit, base=2)
         
-    binary_secret_data = to_binary(secret_data)
- 
-    for i, bit in enumerate(binary_secret_data):
-        i += 171
-        new_byte = int(to_binary(new_image[i])[:-1] + bit, base=2)
-        print(bit, to_binary(image[i]), to_binary(new_byte))
-        new_image[i] = new_byte
-    
-    return bytes(new_image)
+    return bytes(image)
 
-with open("image.png", "rb") as file:
+def decode(encoded_image: bytes) -> bytes:
+    image = bytearray(encoded_image)
+    
+    start_index, end_index = get_png_body_position(image)
+    secret_data_binary = ""
+    
+    # make bytes created here and end scan as soon as stop byte is found 
+    zero_checker = 0
+    for i in range(end_index, start_index, -1):
+        bit = image[i] & 0b00000001
+        secret_data_binary += str(bit)
+        
+    secret_data = bytes([int(secret_data_binary[i:i-8], 2) for i in range(0, len(secret_data_binary), 8)])
+    
+    if secret_data.endswith(Config.STOP_CODE):
+        secret_data = secret_data[:-len(Config.STOP_CODE)]
+    
+    return secret_data
+
+# with open("image.png", "rb") as file:
+#     image = file.read()
+    
+# new_image = encode(image, b'Hello World')
+
+# with open("image2.png", "wb") as file:
+#     file.write(new_image)
+    
+with open("image2.png", "rb") as file:
     image = file.read()
 
-new_image = encode(image, b'Hello World')
-
-with open("image2.png", "wb") as file:
-    file.write(new_image)
+print(decode(image))
